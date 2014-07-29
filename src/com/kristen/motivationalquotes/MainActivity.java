85443a3a2ac73b3976d626bb11243118d232a125
Kristen <kristenlaw.2014@gmail.com>
2014-07-29 16:38:26 -0400

    first working version

--- src/com/kristen/motivationalquotes/MainActivity.java
+++ src/com/kristen/motivationalquotes/MainActivity.java
@@ -4,13 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private MotivationalQuote mMotivationalQuote = new MotivationalQuote();
+	private TextView mQuoteText;
+	private Button mButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Assign the Views from the layout file
+        mQuoteText = (TextView) findViewById(R.id.textView1);
+        mButton = (Button) findViewById(R.id.button1);
+        
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mQuoteText.setText(mMotivationalQuote.getAQuote());
+            }
+        });
 	}
 
 	@Override
